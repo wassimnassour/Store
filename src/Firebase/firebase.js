45f28d7aac1b969f6cb6780f27e40ecdcdeb7a19
Was <you@example.com>
2020-06-11 18:24:56 +0100

    setup firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const  firebaseConfig = {
+    apiKey: "AIzaSyCqWlltnV4zr5GkVunah0ifALjfkncN3KA",
+    authDomain: "e-sto-6a26d.firebaseapp.com",
+    databaseURL: "https://e-sto-6a26d.firebaseio.com",
+    projectId: "e-sto-6a26d",
+    storageBucket: "e-sto-6a26d.appspot.com",
+    messagingSenderId: "259463140835",
+    appId: "1:259463140835:web:a2385cd9452ea44bde7cd6",
+    measurementId: "G-Y03BZLGMT7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
